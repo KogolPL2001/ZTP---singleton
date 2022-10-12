58529339f9cd0c39048f02c66cb73b352c1b8851
KogolPL2001 <kornelg2001@wp.pl>
2022-10-12 15:11:12 +0200

    Tests

--- Singleton/src/App.java
+++ Singleton/src/App.java
@@ -1,23 +1,43 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println(TestSetGet());
+        System.out.println(TestHashCode());
+    }
+    public static String TestSetGet(){
         IPolaczenie pol1,pol2,pol3,pol4;
         pol1=Baza.getPolaczenie();
         pol2=Baza.getPolaczenie();
         pol3=Baza.getPolaczenie();
         pol4=Baza.getPolaczenie();
         pol1.set(1, 'x');
-        System.out.println(pol1.get(1));
-        System.out.println(pol2.get(1));
-        System.out.println(pol3.get(1));
-        System.out.println(pol4.get(1));
-        pol3.set(1, 'y');
-        System.out.println(pol1.get(1));
-        System.out.println(pol2.get(1));
-        System.out.println(pol3.get(1));
-        System.out.println(pol4.get(1));
-        System.out.println(pol1.hashCode());
-        System.out.println(pol2.hashCode());
-        System.out.println(pol3.hashCode());
-        System.out.println(pol4.hashCode());
+        pol2.set(2,'y');
+        String odpowiedz;
+        if('x'==pol3.get(1)&&'y'==pol4.get(2))
+        {
+            odpowiedz= "Polaczenia korzystaja z tej samej bazy";
+        }
+        else
+        {
+            odpowiedz= "Polaczenia nie korzystaja z tej samej bazy";
+        }
+        return odpowiedz;
+
+    }
+    public static String TestHashCode(){
+        IPolaczenie pol1,pol2,pol3,pol4;
+        pol1=Baza.getPolaczenie();
+        pol2=Baza.getPolaczenie();
+        pol3=Baza.getPolaczenie();
+        pol4=Baza.getPolaczenie();
+        String odpowiedz;
+        if(pol1.hashCode()==pol4.hashCode())
+        {
+            odpowiedz= "Polaczenia sa uzywane w kolejnosci";
+        }
+        else
+        {
+            odpowiedz= "Polaczenia nie sa uzywane w kolejnosci";
+        }
+        return odpowiedz;
     }
 }
